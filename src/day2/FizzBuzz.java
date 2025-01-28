3ad620b598ed16beea9304490f973b98f9d370ff
Lijalin2025 <lijalinm@gmail.com>
2025-01-28 18:45:21 +0530

    1stProgram

--- src/day2/FizzBuzz.java
+++ src/day2/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
     public static void main(String[] args) {
 
 
-        for (int i = 0; i <= 15; i++) {
+        for (int i = 0; i <= 100; i++) {
             if (i % 3 == 0 & i % 5 == 0)
             {
                 System.out.println("FizzBuzz");
---
+++ src/day2/Palindrome.java
@@ -0,0 +1,24 @@
+package day2;
+
+import java.util.Scanner;
+
+public record Palindrome() {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your input : ");
+        String input = sc.next();
+        input = input.toLowerCase();
+        StringBuilder rev_input =new StringBuilder(input);
+        StringBuilder newIN = rev_input.reverse();
+        if(input.equals(String.valueOf(newIN)))
+        {
+            System.out.println("it's a Palindrome string");
+        }
+        else
+        {
+            System.out.println("It's not Palindrome string");
+        }
+
+
+    }
+}
